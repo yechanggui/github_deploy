adcffd46e8ea05732e6985c2c84ad9b63c0322a7
yecg <450476961@qq.com>
2020-06-24 14:17:20 +0800

    add a

---
+++ a.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   a.py
+@Time    :   2020/06/24 14:16:03
+@Author  :  Vincent Ye 
+@Version :   1.0
+@Contact :   450476961@qq.com
+@Desc    :   None
+'''
+
+print('111111111')
